6cf71cd25ddf673a928657ed1238b811afc5e6e7
chentian <chentian@kayak.com>
2022-08-10 15:00:42 +0800

    修改

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Game from '../views/Game.vue'
-import Sf from '../views/Sf.vue'
-import Createjs from '../views/Createjs.vue'
 import Run from '../views/Run.vue'
 import Canvas from '../views/Canvas/index.vue'
 import Ball from '../views/Canvas/ball.vue'
@@ -28,8 +26,6 @@ import BoundBounce from '../views/Canvas/boundBounce.vue'
 const routes = [
   { path: '/', component: Home },
   { path: '/game', component: Game },
-  { path: '/sf', component: Sf },
-  { path: '/createjs', component: Createjs },
   { path: '/run', component: Run },
   {
     path: '/canvas',
--- src/views/Createjs.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Createjs></Createjs>
-</template>
-
-<script setup>
-import Createjs from '../components/Createjs.vue'
-</script>
