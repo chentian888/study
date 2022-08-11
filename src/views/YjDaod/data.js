45f6f30ec52334ae5bad3911f2fe04bd0afc4668
chentian <chentian@kayak.com>
2022-08-11 15:38:15 +0800

    场景初始化

--- src/views/YjDaod/data.js
+++ src/views/YjDaod/data.js
@@ -1,7 +1,7 @@
+// sence1
 export const page1Img = [
   'src/assets/img/yjdaod/bg.jpg',
   'src/assets/img/yjdaod/ewm.png',
-  'src/assets/img/yjdaod/loadingBg.jpg',
   'src/assets/img/yjdaod/logo.png',
   'src/assets/img/yjdaod/musicoff.png',
   'src/assets/img/yjdaod/musicon.png',
@@ -15,27 +15,80 @@ export const page1Img = [
   'src/assets/img/yjdaod/p1-star.png',
   'src/assets/img/yjdaod/p1-tree.png'
 ]
+
+// sence2
 export const page2Img = [
-  '~/assets/img/p2-1.png',
-  '~/assets/img/p2-boy.png',
-  '~/assets/img/p2-huati.png',
-  '~/assets/img/p2-lotsofyinfu.png',
-  '~/assets/img/p2-mother.png',
-  '~/assets/img/p2-school.png', // 5
-  '~/assets/img/p2-shitou.png',
-  '~/assets/img/p2-tree4.png',
-  '~/assets/img/p2-wutai.png',
-  '~/assets/img/p2-yinfu.png'
+  'src/assets/img/yjdaod/p2-1.png',
+  'src/assets/img/yjdaod/p2-boy.png',
+  'src/assets/img/yjdaod/p2-huati.png',
+  'src/assets/img/yjdaod/p2-lotsofyinfu.png',
+  'src/assets/img/yjdaod/p2-mother.png',
+  'src/assets/img/yjdaod/p2-school.png', // 5
+  'src/assets/img/yjdaod/p2-shitou.png',
+  'src/assets/img/yjdaod/p2-tree4.png',
+  'src/assets/img/yjdaod/p2-wutai.png',
+  'src/assets/img/yjdaod/p2-yinfu.png'
 ]
 for (let i = 1; i <= 34; i++) {
-  console.log(i)
-  page2Img.push(`~/assets/img/w${i}.png`)
+  page2Img.push(`src/assets/img/yjdaod/w${i}.png`)
 }
 
-export const page3Img = ['~/assets/img/p3-1.png', '~/assets/img/p3-2.png', '~/assets/img/p3-3.png', '~/assets/img/p3-childbirth.png']
-export const page4Img = ['~/assets/img/p4-1.png', '~/assets/img/p4-bg.png', '~/assets/img/p4-house3.png', '~/assets/img/p4-start.png']
+// sence3
+export const page3Img = [
+  'src/assets/img/yjdaod/p3-1.png',
+  'src/assets/img/yjdaod/p3-2.png',
+  'src/assets/img/yjdaod/p3-3.png',
+  'src/assets/img/yjdaod/p3-childbirth.png'
+]
 
-export const page1Ele = []
-export const page2Ele = []
-export const page3Ele = []
-export const page4Ele = []
+// sence4
+export const page4Img = [
+  'src/assets/img/yjdaod/p4-1.png',
+  'src/assets/img/yjdaod/p4-bg.png',
+  'src/assets/img/yjdaod/p4-house3.png',
+  'src/assets/img/yjdaod/p4-start.png'
+]
+for (let i = 1; i <= 52; i++) {
+  page4Img.push(`src/assets/img/yjdaod/x${i}.png`)
+}
+
+export const spriteGroupBgEle = [{ img: page1Img[0], x: 0, y: 0, alpah: 1, sprName: 'bgSpr', sprGroup: 'spriteGroupBg' }]
+
+export const page1Ele = [
+  { img: page1Img[5], x: 0, y: 0, sprName: 'p1Bg', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[6], x: -20, y: 177, sprName: 'p1Cloud1', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[7], x: 725, y: 0, sprName: 'p1Cloud2', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[8], x: 0, y: 1093, sprName: 'p1Grass1', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[9], x: 836, y: 1161, sprName: 'p1Grass2', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[10], x: 732, y: 0, sprName: 'p1House', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[11], x: 996, y: 600, sprName: 'p1P1', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[12], x: 424, y: 419, sprName: 'p1Star', sprGroup: 'spriteGroupSences/sence1', alpah: 1 },
+  { img: page1Img[13], x: -20, y: 604, sprName: 'p1Tree', sprGroup: 'spriteGroupSences/sence1', alpah: 1 }
+]
+
+export const page2Ele = [
+  { img: page2Img[0], x: 0, y: 816, sprName: 'p21', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[1], x: 1508, y: 262, sprName: 'p2Boy', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[2], x: 1298, y: 414, sprName: 'p2Huati', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[3], x: 1932, y: 307, sprName: 'p2Lotsofyinfu', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[4], x: 144, y: 768, sprName: 'p2Mother', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[5], x: 613, y: 31, sprName: 'p2School', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[6], x: 1200, y: 1149, sprName: 'p2Shitou', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[7], x: 1932, y: 49, sprName: 'p2Tree4', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[8], x: 2243, y: 349, sprName: 'p2Wutai', sprGroup: 'spriteGroupSences/sence2', alpah: 1 },
+  { img: page2Img[9], x: 3009, y: 273, sprName: 'p2Yinfu', sprGroup: 'spriteGroupSences/sence2', alpah: 1 }
+]
+
+export const page3Ele = [
+  { img: page3Img[0], x: 0, y: 80, sprName: 'p31', sprGroup: 'spriteGroupSences/sence3', alpah: 1 },
+  { img: page3Img[1], x: 826, y: 142, sprName: 'p32', sprGroup: 'spriteGroupSences/sence3', alpah: 1 },
+  { img: page3Img[2], x: 971, y: 24, sprName: 'p33', sprGroup: 'spriteGroupSences/sence3', alpah: 1 },
+  { img: page3Img[3], x: 2397, y: 453, sprName: 'p3Childbirth', sprGroup: 'spriteGroupSences/sence3', alpah: 1 }
+]
+
+export const page4Ele = [
+  { img: page4Img[0], x: 691, y: 529, sprName: 'p41', sprGroup: 'spriteGroupSences/sence4', alpah: 1 },
+  { img: page4Img[1], x: 588, y: 0, sprName: 'p4Bg', sprGroup: 'spriteGroupSences/sence4', alpah: 1 },
+  { img: page4Img[2], x: 0, y: 0, sprName: 'p4House3', sprGroup: 'spriteGroupSences/sence4', alpah: 1 },
+  { img: page4Img[3], x: 1398, y: 0, sprName: 'p4Start', sprGroup: 'spriteGroupSences/sence4', alpah: 1 }
+]
--- src/views/YjDaod/index.vue
+++ src/views/YjDaod/index.vue
@@ -4,17 +4,18 @@
 
 <script setup>
 import { onMounted } from 'vue'
-import { Application, Loader } from 'pixi.js'
-import { page1Img } from './data.js'
-console.log()
-const stage = new Application({
+import { Application, Loader, Container, Sprite } from 'pixi.js'
+import AlloyTouch from 'alloytouch'
+import { page1Img, page2Img, page3Img, page4Img, spriteGroupBgEle, page1Ele, page2Ele, page3Ele, page4Ele } from './data.js'
+
+const app = new Application({
   width: 750,
   height: 1440
 })
 
 onMounted(() => {
   const box = document.querySelector('.grow-box')
-  box.appendChild(stage.view)
+  box.appendChild(app.view)
   load()
 })
 
@@ -22,7 +23,7 @@ function load() {
   // const loader = new Loader()
   // loader.add('spritesheet', 'https://pixijs.io/examples/examples/assets/spritesheet/mc.json').load(onAssetsLoaded)
   const loader = new Loader()
-  loader.add(page1Img)
+  loader.add(page1Img).add(page2Img).add(page3Img).add(page4Img)
   loader.load((loader, resources) => {
     console.log('loader===', loader)
     console.log('resources===', resources)
@@ -30,10 +31,115 @@ function load() {
   loader.onProgress.add(({ progress }) => {
     console.log('progress', progress)
   })
+  loader.onComplete.add(() => {
+    onAssetsLoaded()
+  })
 }
 
+// init container and set touch event
 function onAssetsLoaded() {
-  console.log('232323')
+  // bg container
+  const spriteGroupBg = new Container()
+  spriteGroupBg.name = 'spriteGroupBg'
+  spriteGroupBg.position.set(0, 0)
+  app.stage.addChild(spriteGroupBg)
+
+  // sence container
+  const spriteGroupSences = new Container()
+  spriteGroupSences.name = 'spriteGroupSences'
+  spriteGroupSences.position.set(0, 0)
+  app.stage.addChild(spriteGroupSences)
+  // sence1
+  const sence1 = new Container()
+  sence1.position.set(0, 0)
+  sence1.name = 'sence1'
+
+  // sence2
+  const sence2 = new Container()
+  sence2.position.set(1773, 0)
+  sence2.name = 'sence2'
+
+  // sence3
+  const sence3 = new Container()
+  sence3.position.set(4960, 0)
+  sence3.name = 'sence3'
+
+  // sence4
+  const sence4 = new Container()
+  sence4.position.set(7902, 0)
+  sence4.name = 'sence4'
+
+  spriteGroupSences.addChild(sence1)
+  spriteGroupSences.addChild(sence2)
+  spriteGroupSences.addChild(sence3)
+  spriteGroupSences.addChild(sence4)
+
+  // last page container
+  const spriteGroupLast = new Container()
+  spriteGroupLast.name = 'spriteGroupLast'
+  spriteGroupLast.position.set(0, 0)
+  app.stage.addChild(spriteGroupLast)
+
+  const imgArr = []
+  imgArr.push(...spriteGroupBgEle)
+  imgArr.push(...page1Ele)
+  imgArr.push(...page2Ele)
+  imgArr.push(...page3Ele)
+  imgArr.push(...page4Ele)
+
+  imgArr.forEach((ele) => {
+    addSprToGroup(ele)
+  })
+
+  bindTouchAction()
+}
+
+// set the element to the corresponding scene
+function addSprToGroup({ img = '', x = '', y = '', sprName = '', sprGroup = '', alpah = '' }) {
+  const sprite = new Sprite.from(img)
+  sprite.position.set(x, y)
+  sprite.name = sprName
+  sprite.alpah = alpah
+
+  const containers = sprGroup.split('/')
+  let parent = app.stage.getChildByName(containers[0])
+
+  const len = containers.length
+  if (len > 1) {
+    for (let i = 1; i < len; i++) {
+      parent = parent.getChildByName(containers[i])
+    }
+  }
+
+  parent.addChild(sprite)
+}
+
+// bind touch action
+function bindTouchAction() {
+  const min = -10800 - 750
+  const alloyTouch = new AlloyTouch({
+    touch: '.grow-box', //反馈触摸的dom
+    vertical: true, //不必需，默认是true代表监听竖直方向touch
+    property: 'translateY', //被运动的属性
+    min: min, //不必需,运动属性的最小值
+    max: 0, //不必需,滚动属性的最大值
+    sensitivity: 0.1, //不必需,触摸区域的灵敏度，默认值为1，可以为负数
+    factor: 10, //不必需,表示触摸位移运动位移与被运动属性映射关系，默认值是1
+    moveFactor: 10, //不必需,表示touchmove位移与被运动属性映射关系，默认值是1
+    bindSelf: false,
+    maxSpeed: 2, //不必需，触摸反馈的最大速度限制
+    value: 0,
+    change: function (value) {
+      const progress = value / min
+      console.log(value, progress)
+    },
+    touchStart: function (evt, value) {},
+    touchMove: function (evt, value) {},
+    touchEnd: function (evt, value) {},
+    tap: function (evt, value) {},
+    pressMove: function (evt, value) {},
+    animationEnd: function (value) {} //运动结束
+  })
 }
 </script>
 
